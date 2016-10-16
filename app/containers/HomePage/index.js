4bd88f1079d335521ba311119c3724acefbc01b7
romy <rosemarie.reisenberger@yahoo.de>
2016-10-16 23:12:15 +0200

    Remove comments from React Boilerplate
    I don't need those helpful texts anymore.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,14 +1,3 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
 // Plugin needed for Material-UI until the official React version is released
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
