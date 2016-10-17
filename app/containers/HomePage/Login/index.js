7cc81eb68da50a7b505a8669885af646e4a45241
romy <rosemarie.reisenberger@yahoo.de>
2016-10-17 22:10:39 +0200

    Add onClick function to `Login`
    Use a suffix for `getImgurToken` to solve the no-shadow linting conflict.
    
    Although the prop types for `getImgurToken` are defined through
    a recompose function, the linter indicates a false error.

--- app/containers/HomePage/Login/index.js
+++ app/containers/HomePage/Login/index.js
@@ -1,18 +1,28 @@
 import ActionExitToApp from 'material-ui/svg-icons/action/exit-to-app';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { defaultProps, setDisplayName, setPropTypes } from 'recompose';
 import RaisedButton from 'material-ui/RaisedButton';
 import React from 'react';
 import R from 'ramda';
 
+import getImgurTokenAction from '../../../interactions/imgur/actions';
 
-const Login = () =>
+
+const Login = ({ getImgurToken }) => // eslint-disable-line react/prop-types
   <RaisedButton
     label="login with imgur"
     fullWidth
     style={{ marginTop: '15%' }}
+    onClick={getImgurToken}
   >
     <ActionExitToApp />
   </RaisedButton>;
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({
+    getImgurToken: getImgurTokenAction,
+  }, dispatch);
 
 const enhance = R.pipe(
   defaultProps({
@@ -21,6 +31,7 @@ const enhance = R.pipe(
   setPropTypes({
     getImgurToken: React.PropTypes.func,
   }),
+  connect(null, mapDispatchToProps),
   setDisplayName('Login'),
 );
 
