9286419e9fd1cb56f6291c761cb8a1cc4dbf9ab5
romy <rosemarie.reisenberger@yahoo.de>
2016-10-23 21:18:10 +0200

    Extend `HomePage` to render albums of a user
    ... when a username was found.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -2,20 +2,45 @@
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
+import { connect } from 'react-redux';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
 
+import Albums from './Albums';
+import { selectUsername } from 'interactions/user/selectors';
 import Welcome from './Welcome';
 
-export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   render() {
+    let children;
+
+    if (this.props.username) {
+      children = <Albums />;
+    } else {
+      children = <Welcome />;
+    }
+
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-        <Welcome />
+        {children}
       </MuiThemeProvider>
     );
   }
 }
+
+HomePage.propTypes = {
+  username: React.PropTypes.string,
+};
+
+const mapStateToProps = (state) =>
+  ({
+    username: selectUsername(state),
+  });
+
+
+export { HomePage };
+export default connect(mapStateToProps)(HomePage);
--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -2,7 +2,7 @@ import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
 
-import HomePage from '../index';
+import { HomePage } from '../index';
 
 
 describe('<HomePage />', () => {
@@ -12,9 +12,15 @@ describe('<HomePage />', () => {
     expect(app.find('MuiThemeProvider').length).toEqual(1);
   });
 
-  it('should render Welcome', () => {
+  it('should render Welcome when no username is found', () => {
     const app = shallow(<HomePage />);
 
     expect(app.find('Welcome').length).toEqual(1);
+  });
+
+  it('should render Albums when a username is found', () => {
+    const app = shallow(<HomePage username="Mio" />);
+
+    expect(app.find('Albums').length).toEqual(1);
   });
 });
