69579c16a78bb42303b4487699bc60d171272cd2
romy <rosemarie.reisenberger@yahoo.de>
2016-10-23 20:42:19 +0200

    Add component `AlbumCard`
    Use component `Card` from Material UI to display an album.

---
+++ app/containers/HomePage/Albums/album-card.js
@@ -0,0 +1,57 @@
+import { Card, CardActions, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setDisplayName, setPropTypes } from 'recompose';
+import RaisedButton from 'material-ui/RaisedButton';
+import React from 'react';
+import R from 'ramda';
+
+import { getAlbumImages as getAlbumImagesAction } from 'interactions/albums/actions';
+
+
+const AlbumCard = ({ title, src, description, getAlbumImages, id }) => // eslint-disable-line react/prop-types
+  <Card style={{ margin: '5%' }}>
+    <CardMedia overlay={<CardTitle title={title} />} >
+      <img src={src} alt={title} />
+    </CardMedia>
+    <CardActions>
+      <RaisedButton
+        label="View all images"
+        fullWidth
+        onClick={() => getAlbumImages({ id })}
+      />
+    </CardActions>
+    <CardText>
+      {description}
+    </CardText>
+  </Card>;
+
+const mapStateToProps = (state, props) => {
+  const album = state.getIn(['albums', props.id]);
+  const imageId = album.get('cover');
+
+  return ({
+    title: album.get('title'),
+    src: `http://i.imgur.com/${imageId}.png`,
+    description: album.get('description'),
+  });
+};
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({
+    getAlbumImages: getAlbumImagesAction,
+  }, dispatch);
+
+const enhance = R.pipe(
+  setPropTypes({
+    src: React.PropTypes.string,
+    description: React.PropTypes.string,
+    title: React.PropTypes.string,
+  }),
+  connect(mapStateToProps, mapDispatchToProps),
+  setDisplayName('AlbumCard'),
+);
+
+
+export { AlbumCard };
+export default enhance(AlbumCard);
